8df84367c3d98eadeab93a21aed770e796e5efc2
damiencrj <damiencrj.study@gmail.com>
2026-05-09 22:06:43 +0800

    Update Search FRA History for Fundraiser script to match User Story Diagrams
    Update Search FRA History for Fundraiser script to match User Story Diagrams

--- fund_raiser/controller/FundRaiserSearchFraHisC.py
+++ fund_raiser/controller/FundRaiserSearchFraHisC.py
@@ -1,11 +1,16 @@
 from fund_raiser.controller.FundRaiserViewFraC import FundRaiserViewFraC
-
+from fund_raiser.entity.FRA import FRA
 
 class FundRaiserSearchFraHisC:
     @staticmethod
     def searchFraHis(query, owner_id):
-        return [
+        #return [
+        #    fra
+        #    for fra in FundRaiserViewFraC.searchFra(query, owner_id)
+        #    if fra.get("fra_status") == "completed"
+        #]
+        return[
             fra
-            for fra in FundRaiserViewFraC.searchFra(query, owner_id)
+            for fra in FRA.searchFra(query, owner_id)
             if fra.get("fra_status") == "completed"
         ]
